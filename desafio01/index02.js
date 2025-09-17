8c9b166963faf0fef3222070ee095b17ff68b7c3
Amanda Dimas Pessoa <107128558+amandadimas@users.noreply.github.com>
2025-09-17 19:47:38 -0300

    Update index02.js

--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,69 @@
-console.log("Hello, World! 002");
+// Abstract Factory
+// Produtos - Botões
+class LightButton {
+    render() {
+      return "Botão branco criado";
+    }
+  }
+  
+  class DarkButton {
+    render() {
+      return "Botão preto criado";
+    }
+  }
+  
+  // Produtos - Janelas
+  class LightWindow {
+    render() {
+      return "Janela clara aberta";
+    }
+  }
+  
+  class DarkWindow {
+    render() {
+      return "Janela escura aberta";
+    }
+  }
+  
+  // Abstract Factory
+  class UIFactory {
+    createButton() {}
+    createWindow() {}
+  }
+  
+  // Concrete Factory - Tema Claro
+  class LightFactory extends UIFactory {
+    createButton() {
+      return new LightButton();
+    }
+    createWindow() {
+      return new LightWindow();
+    }
+  }
+  
+  // Concrete Factory - Tema Escuro
+  class DarkFactory extends UIFactory {
+    createButton() {
+      return new DarkButton();
+    }
+    createWindow() {
+      return new DarkWindow();
+    }
+  }
+  
+  // Client
+  const theme = "dark";
+  let factory;
+  
+  if (theme === "light") {
+    factory = new LightFactory();
+  } else {
+    factory = new DarkFactory();
+  }
+  
+  const button = factory.createButton();
+  const windowUI = factory.createWindow();
+  
+  console.log(button.render());
+  console.log(windowUI.render());
+  
